cde17e743b6afb29a7c7544900569ff16c3f24f6
Indefyn <oud3noida@naver.com>
2020-12-23 17:13:11 +0900

    Update main.cpp

--- 6603/main.cpp
+++ 6603/main.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int n;
 const int MAX = 6;
-vector<int> permutation, numbers;
-
-void loop_permutation(int start, int depth) {
-    if (depth >= MAX) {
-        for (int i=0; i<MAX; ++i) cout << permutation[i] << " ";
-        return;
-    }
-    for (int i=start; i<=n-MAX+depth; ++i) {
-        permutation[depth] = numbers[i];
-        loop_permutation(i+1, depth+1);
-        if (i < n-MAX+depth) cout << "\n";
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    permutation.assign(6, 0);
-
+    int n;
     cin >> n;
     while (n != 0) {
-        numbers.assign(n, 0);
+        vector<int> numbers(n, 0), permutation(n, 0);
         for (int i=0; i<n; ++i) cin >> numbers[i];
+        for (int i=MAX; i<n; ++i) permutation[i] = 1;
 
-        loop_permutation(0, 0);
+        while (true) {
+            for (int i=0; i<n; ++i)
+                if (permutation[i] == 0) cout << numbers[i] << " ";
+            if (next_permutation(permutation.begin(), permutation.end()))
+                cout << "\n";
+            else break;
+        }
 
         cin >> n;
         if (n != 0) cout << "\n\n";
