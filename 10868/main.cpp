1bf83f4f04464215bcc0e25cb5491951c52a90c3
Indefyn <oud3noida@naver.com>
2021-02-13 18:36:59 +0900

    RMQ Using Fenwick Trees

--- 10868/main.cpp
+++ 10868/main.cpp
@@ -22,42 +22,6 @@ private:
             return i - (i & ~(i - 1));
     }
 
-    vector<int> child(int i, TreeType tree_type) {
-        vector<int> ret;
-        auto other_tree_type = (tree_type == Tree1) ? Tree2 : Tree1;
-
-        int upper_bound_i = parent(i, Tree1);
-        int lower_bound_i = parent(i, Tree2);
-        i = (tree_type == Tree1) ? i - 1 : i + 1;
-        while (in_range(i) && lower_bound_i < i && i < upper_bound_i) {
-            ret.push_back(i);
-            i = parent(i, other_tree_type);
-        }
-
-        return ret;
-    }
-
-    void update(int index, int value, int old_value, TreeType tree_type) {
-        auto &tree = (tree_type == Tree1) ? tree1 : tree2;
-
-        while (in_range(index)) {
-            if (value > tree[index]) {
-                if (old_value > tree[index])
-                    break;
-
-                value = min(value, real[index - 1]);
-                for (int child_index : child(index, tree_type)) {
-                    value = min(value, tree[child_index]);
-                }
-            }
-
-            if (value == tree[index])
-                break;
-            tree[index] = value;
-            index = parent(index, tree_type);
-        }
-    }
-
 public:
     MinFenwickTree(vector<int> &data)
     : tree_size(data.size()), real(data) {
@@ -99,38 +63,24 @@ public:
         ret = min(ret, real[i - 1]);
         return ret;
     }
-
-    void update(int index, int value) {
-        int old_value = real[index - 1];
-        real[index - 1] = value;
-
-        update(index, value, old_value, Tree1);
-        update(index, value, old_value, Tree2);
-    }
 };
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
+    int N, num_queries;
+    cin >> N >> num_queries;
 
     vector<int> arr(N, INF);
     for (int i = 0; i < N; ++i)
         cin >> arr[i];
     auto tree = new MinFenwickTree(arr);
 
-    int num_queries;
-    cin >> num_queries;
-
-    int command_type, a, b;
+    int begin, end;
     for (int i = 0; i < num_queries; ++i) {
-        cin >> command_type >> a >> b;
-        if (command_type == 1)
-            tree->update(a, b);
-        else
-            cout << tree->query(a, b) << '\n';
+        cin >> begin >> end;
+        cout << tree->query(begin, end) << '\n';
     }
 
     return 0;
