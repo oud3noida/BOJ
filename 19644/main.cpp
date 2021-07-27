795e5f39c16e48e1f715b62926c90b55028f51eb
oud3noida <51070450+oud3noida@users.noreply.github.com>
2021-07-27 10:41:16 +0900

    Update main.cpp

--- 19644/main.cpp
+++ 19644/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int L, range, damage, claymore, usedClaymore, curZombie;
+vector<int> positionUsedClaymore;
 
 int main()
 {
@@ -13,8 +15,14 @@ int main()
     usedClaymore = 0;
     for (int i=0; i<L; ++i) {
         cin >> curZombie;
-        curZombie -= damage * (min(range, i+1) - usedClaymore);
-        if (curZombie > 0) ++usedClaymore;
+        auto a = positionUsedClaymore.begin(),
+             b = positionUsedClaymore.end();
+        int numNotShot = b - lower_bound(a, b, i - range + 1);
+        curZombie -= damage * (min(range, i+1) - numNotShot);
+        if (curZombie > 0) {
+            ++usedClaymore;
+            positionUsedClaymore.push_back(i);
+        }
         if (usedClaymore > claymore) {
             cout << "NO";
             return 0;
