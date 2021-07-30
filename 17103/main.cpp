b630db5a9cf449e68a9b0cfc69d23715fdb5d35d
oud3noida <51070450+oud3noida@users.noreply.github.com>
2021-07-30 21:31:25 +0900

    Create main.cpp

---
+++ 17103/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+constexpr int MAX = 1'000'000;
+int num_queries, n;
+vector<bool> is_prime;
+vector<int> odd_prime_numbers;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    is_prime.assign(MAX + 1, false);
+    
+    for (int i=3; i<=MAX; i+=2) {
+        is_prime[i] = true;
+        for (int p : odd_prime_numbers) {
+            if (p > sqrt(i)) break;
+            if (i % p == 0) {
+                is_prime[i] = false;
+                break;
+            }
+        }
+        if (is_prime[i]) odd_prime_numbers.push_back(i);
+    }
+    
+    cin >> num_queries;
+    
+    for (int q=0; q<num_queries; ++q) {
+        cin >> n;
+        
+        int num_goldbach_partitions = 0;
+        for (int p : odd_prime_numbers) {
+            if (p > n/2) break;
+            if (is_prime[n-p]) ++num_goldbach_partitions;
+        }
+        
+        cout << num_goldbach_partitions << '\n';
+    }
+    
+    return 0;
+}
