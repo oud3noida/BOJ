ff353b1f88092e2c5de199c9e391e3429100cebd
oud3noida <51070450+oud3noida@users.noreply.github.com>
2021-09-09 20:56:45 +0900

    Update with More Efficient Algorithm

--- 15591/main.cpp
+++ 15591/main.cpp
@@ -7,20 +7,7 @@ using namespace std;
 
 constexpr int INF = numeric_limits<int>::max();
 int n, num_queries;
-vector<vector<int>> adj, relevance, children;
-vector<int> parent;
-vector<bool> visited;
-
-void dfs(int cur) {
-    for (int child : adj[cur]) {
-        if (!visited[child]) {
-            visited[child] = true;
-            children[cur].push_back(child);
-            parent[child] = cur;
-            dfs(child);
-        }
-    }
-}
+vector<vector<int>> adj, relevance;
 
 int main()
 {
@@ -40,49 +27,32 @@ int main()
         relevance[b-1][a-1] = r;
     }
     
-    parent.assign(n, -1);
-    children.assign(n, vector<int>());
-    visited.assign(n, false);
-    visited[0] = true;
-    dfs(0);
-    
-    for (int i = 0; i < n; i++) {
-        queue<pair<int, int>> q;
-        q.emplace(i, INF);
-        while (!q.empty()) {
-            auto [cur, cur_relevance] = q.front();
-            q.pop();
-            
-            for (int child : children[cur]) {
-                relevance[i][child] = min(cur_relevance, relevance[cur][child]);
-                relevance[child][i] = relevance[i][child];
-                q.emplace(child, relevance[i][child]);
-            }
-        }
-    }
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (relevance[i][j] != INF) continue;
-            int common_parent = parent[i];
-            while (relevance[common_parent][j] == INF) {
-                common_parent = parent[common_parent];
-            }
-            relevance[i][j] = min(relevance[common_parent][i], relevance[common_parent][j]);
-            relevance[j][i] = relevance[i][j];
-        }
-    }
-    
     int k, x;
     for (int query = 0; query < num_queries; query++) {
         cin >> k >> x;
         
         int ans = 0;
-        for (int rel : relevance[x-1]) {
-            if (rel >= k) ++ans;
+        
+        vector<bool> visited(n, false);
+        queue<int> q;
+        visited[x-1] = true;
+        q.push(x-1);
+        
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            
+            ++ans;
+            
+            for (int neighbor : adj[cur]) {
+                if (visited[neighbor]) continue;
+                if (relevance[cur][neighbor] < k) continue;
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
         }
         
-        cout << ans-1 << '\n';
+        cout << ans - 1 << '\n';
     }
     
     return 0;
